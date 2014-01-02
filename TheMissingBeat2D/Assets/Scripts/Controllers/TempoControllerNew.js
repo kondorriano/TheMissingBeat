eb457061b1c12b979be596ac82f2b051e4f3cbde
Nicolás Delgado <nicobito@gmail.com>
2014-01-02 23:59:26 +0100

    Patatas

---
+++ TheMissingBeat2D/Assets/Scripts/Controllers/TempoControllerNew.js
@@ -0,0 +1,119 @@
+#pragma strict
+
+public var finish : AudioClip;
+private var finished : boolean = false;
+
+private var beatObjects : GameObject;
+private var ballObject : GameObject;
+private var beatsPerMinute : int = 90;
+private var beatSamples : int = 44100*60/beatsPerMinute;
+
+public var timeOffset : float = 0;
+private var timeSamplesOffset : int = 0;
+
+private var lastMusicDiv : int = 0;
+private var shouldFinish : boolean = false;
+
+private var changedFrequency : boolean = false;
+private var finishTime : float;
+private var timePlay : float;
+private var lifeTime : int;
+
+private var freq : int = 2;
+
+private var myName : String = "Someone";
+private var rankeado : boolean = false;
+
+//New
+
+//private var loopDrum : float[];
+public var audioM : AudioSource;
+//private var audioD : AudioSource;
+//private var drumPosition : int = 1;
+
+//private var lastMusicDivD : int = 0;
+
+private var beatTime : float = 0;
+private var beatCount : int = -1;
+
+
+
+function Start () {
+	beatObjects = GameObject.FindGameObjectWithTag("BeatMaster");
+	ballObject = GameObject.FindGameObjectWithTag("Gear");
+	audioM.Play();	
+}
+
+function Update () {
+	//Avanza el tiempo
+	beatTime += Time.deltaTime;
+	//Cuando llegamos a los 60 segundos de cancion ira el doble de rapido
+	if (!changedFrequency && audioM.time > 60) BroadcastFrequency(--freq);
+
+	//Si hacemos un beat
+	if(beatTime > 60.0/beatsPerMinute) {
+		beatTime -= 60.0/beatsPerMinute;
+		++beatCount;
+		if (!audioM.isPlaying) {
+			audioM.Play();
+		}
+		BroadcastBeat(beatCount);
+	}
+	
+	timePlay += Time.deltaTime;
+	
+	if (audioM.time > 122) Application.LoadLevel(Application.loadedLevel);
+
+}
+
+function BroadcastBeat(beat : int) {
+	beatObjects.BroadcastMessage("Beat", beat);//OLD, TESTING
+	ballObject.SendMessage("Beat", beat);
+	
+	
+}
+
+function BroadcastFrequency(frequency : float)  {
+	beatObjects.BroadcastMessage("ChangeFrequency", frequency);
+	ballObject.SendMessage("ChangeFrequency", frequency);
+
+	changedFrequency = true;
+}
+
+function OnGUI() {
+	GUI.color = Color.white;
+	lifeTime = audioM.clip.length-audioM.time;
+	if (!finished) {
+		GUI.Label(Rect(10,30,100,20), "LifeTime: "+lifeTime);
+		GUI.Label(Rect(10,50,100,20), "Time: "+timePlay.ToString("F3"));
+
+	} else {
+		GUI.Label(Rect(10,30,200,20), "Your Score: "+finishTime.ToString("F3"));
+		if(!rankeado) {
+			myName = GUI.TextField(Rect (10, 60, 100, 20), myName, 25);		
+			if(GUI.Button(Rect(10, 90, 100, 20), "Submit score")) {
+				//var arr = new Array();
+
+				//arr.push(finishTime.ToString("F2"));
+				//arr.push(myName);
+				//var rank : Ranking = gameObject.GetComponent("Ranking");
+				//rank.Rankea(myName, finishTime);
+				//gameObject.SendMessage("Rankea", arr);
+				rankeado = true;
+			}
+		}
+   		
+	}
+	
+
+}
+
+function Finish() {
+	finishTime = timePlay-lifeTime;
+	audio.Stop();
+	//audioD.Stop();
+	audio.clip = finish;
+	audio.loop = true;
+	audio.Play();
+	finished = true;
+}
---
+++ TheMissingBeat2D/Assets/Scripts/GearRotationScript.js
@@ -0,0 +1,87 @@
+﻿#pragma strict
+
+//El script que hace rotar diferentes engranajes
+//NewGamePlay
+public var activated : boolean = true;
+private var beatActivation : boolean;
+
+//EndNewGameplay
+
+public var freq : int = 1;
+public var offset : int = 0;
+
+public var dientes : float = 4;
+public var sentido : int = 1;
+public var speed : float = 60;
+
+private var rotate : boolean = false;
+private var grados : float = 45;
+
+private var initRotation : Quaternion;
+private var nextRotation : Quaternion;
+
+private var initAngle : float;
+private var nextAngle : float;
+
+
+private var cont : int = 0;
+
+private var activate : boolean = false; //Todo lo relacionado con etso es nuevo
+
+
+function Start () {
+	grados = 360.0/(dientes*2);
+	initRotation = transform.rotation;
+	nextRotation = initRotation;
+	//NewGP
+	beatActivation = activated;
+
+	initAngle = 0;
+}
+private var test : int = 0;
+
+function Update () {
+	if(Input.GetKeyDown(KeyCode.Space)) Beat(++test);
+	Debug.Log(test);
+	//transform.rotation = Quaternion.Slerp(transform.rotation, nextRotation, Time.deltaTime*speed);
+	
+	var nextStep : float  = transform.rotation.z + rigidbody2D.angularVelocity*Time.fixedDeltaTime;
+    var totalRotation : float = nextAngle - nextStep;
+  	while ( totalRotation < -180 * Mathf.Deg2Rad ) totalRotation += 360 * Mathf.Deg2Rad;
+ 	while ( totalRotation >  180 * Mathf.Deg2Rad ) totalRotation -= 360 * Mathf.Deg2Rad;
+  	
+  	var desiredAngularVelocity : float = totalRotation/Time.fixedDeltaTime;
+  	var torque : float = desiredAngularVelocity /Time.fixedDeltaTime;
+  	rigidbody2D.AddTorque(torque);
+}
+
+function Beat(beat : int) {
+		if ((beat + offset)%freq == 0) {
+			rotate = true;	
+			++cont;
+			if(cont == dientes*2) {
+				nextRotation = initRotation;
+				nextAngle = initAngle;
+				cont = 0;
+			} else {
+				nextRotation = initRotation*Quaternion.Euler(0,0,sentido*cont*grados);
+				nextAngle = initAngle + sentido*cont*grados;
+			}
+			//NewGamePlay
+		}
+		
+	
+
+}
+
+function ChangeFrequency(frequency : float) {
+	freq = frequency;
+}
+
+function changeBeat(b : boolean){
+	activate = b;
+}
+
+function changeActivation(){
+	if(beatActivation == activated) beatActivation = !activated;
+}
